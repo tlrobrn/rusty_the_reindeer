46a793acecdbfcab311b0b978048297ab41a76cf
tlrobrn <obrien.taylorm@gmail.com>
2017-12-01 09:47:38 -0600

    cargo fmt

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,8 +1,7 @@
 extern crate rusty_the_reindeer;
 
 fn main() {
-    let contents = rusty_the_reindeer::get_input()
-        .expect("Must provide valid input path");
+    let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
     let digits = to_digits(&contents);
 
     println!("Part 1: {}", solve_captcha1(&digits));
@@ -20,29 +19,23 @@ fn solve_captcha2(digits: &Vec<u32>) -> u32 {
 fn solve_captcha(digits: &Vec<u32>, step: usize) -> u32 {
     let length = digits.len();
 
-    digits
-        .iter()
-        .enumerate()
-        .fold(0, |total, (i, &n)| {
-            let lookup = if i + step < length {
-                i + step
-            } else {
-                i + step - length
-            };
+    digits.iter().enumerate().fold(0, |total, (i, &n)| {
+        let lookup = if i + step < length {
+            i + step
+        } else {
+            i + step - length
+        };
 
-            if n == digits[lookup] {
-                total + n
-            } else {
-                total
-            }
-        })
+        if n == digits[lookup] {
+            total + n
+        } else {
+            total
+        }
+    })
 }
 
 fn to_digits(contents: &str) -> Vec<u32> {
-    contents
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect()
+    contents.chars().map(|c| c.to_digit(10).unwrap()).collect()
 }
 
 
@@ -52,7 +45,7 @@ mod day1_tests {
 
     #[test]
     fn to_digits_returns_vec_of_digits() {
-        assert_eq!(vec![1,1,2,2], to_digits("1122"));
+        assert_eq!(vec![1, 1, 2, 2], to_digits("1122"));
     }
 
     #[test]
@@ -68,7 +61,7 @@ mod day1_tests {
         assert_eq!(6, solve_captcha2(&to_digits("1212")));
         assert_eq!(0, solve_captcha2(&to_digits("1221")));
         assert_eq!(4, solve_captcha2(&to_digits("123425")));
-        assert_eq!(12,solve_captcha2(&to_digits("123123")));
+        assert_eq!(12, solve_captcha2(&to_digits("123123")));
         assert_eq!(4, solve_captcha2(&to_digits("12131415")));
     }
 }
