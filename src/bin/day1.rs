aa13c1e07b180d79cfb4c60d61596d1ddff4a3f4
tlrobrn <obrien.taylorm@gmail.com>
2017-12-01 15:14:03 -0600

    solve both parts at once

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,31 +2,31 @@ extern crate rusty_the_reindeer;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
-    let digits = to_digits(&contents);
+    let (part1, part2) = multi_solve_captcha(&contents);
 
-    println!("Part 1: {}", solve_captcha1(&digits));
-    println!("Part 2: {}", solve_captcha2(&digits));
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn solve_captcha1(digits: &[u32]) -> u32 {
-    solve_captcha(digits, 1)
-}
-
-fn solve_captcha2(digits: &[u32]) -> u32 {
-    solve_captcha(digits, digits.len() / 2)
-}
-
-fn solve_captcha(digits: &[u32], step: usize) -> u32 {
+fn multi_solve_captcha(contents: &str) -> (u32, u32) {
+    let digits = to_digits(contents);
     let length = digits.len();
+    let half = length / 2;
 
-    digits.iter().enumerate().fold(0, |total, (i, &n)| {
-        let lookup = (i + step) % length;
-        if n == digits[lookup] {
-            total + n
-        } else {
-            total
-        }
-    })
+    digits
+        .iter()
+        .enumerate()
+        .fold((0, 0), |(total1, total2), (i, &n)| {
+            match (
+                digits[(i + 1) % length] == n,
+                digits[(i + half) % length] == n,
+            ) {
+                (true, true) => (total1 + n, total2 + n),
+                (true, false) => (total1 + n, total2),
+                (false, true) => (total1, total2 + n),
+                (false, false) => (total1, total2),
+            }
+        })
 }
 
 fn to_digits(contents: &str) -> Vec<u32> {
@@ -42,21 +42,29 @@ mod day1_tests {
     fn to_digits_returns_vec_of_digits() {
         assert_eq!(vec![1, 1, 2, 2], to_digits("1122"));
     }
-
     #[test]
-    fn solve_captcha_works() {
-        assert_eq!(3, solve_captcha1(&to_digits("1122")));
-        assert_eq!(4, solve_captcha1(&to_digits("1111")));
-        assert_eq!(0, solve_captcha1(&to_digits("1234")));
-        assert_eq!(9, solve_captcha1(&to_digits("91212129")));
+    fn part1() {
+        let (x, _) = multi_solve_captcha("1122");
+        assert_eq!(3, x);
+        let (x, _) = multi_solve_captcha("1111");
+        assert_eq!(4, x);
+        let (x, _) = multi_solve_captcha("1234");
+        assert_eq!(0, x);
+        let (x, _) = multi_solve_captcha("91212129");
+        assert_eq!(9, x);
     }
 
     #[test]
-    fn solve_captcha_2_works() {
-        assert_eq!(6, solve_captcha2(&to_digits("1212")));
-        assert_eq!(0, solve_captcha2(&to_digits("1221")));
-        assert_eq!(4, solve_captcha2(&to_digits("123425")));
-        assert_eq!(12, solve_captcha2(&to_digits("123123")));
-        assert_eq!(4, solve_captcha2(&to_digits("12131415")));
+    fn part2() {
+        let (_, x) = multi_solve_captcha("1212");
+        assert_eq!(6, x);
+        let (_, x) = multi_solve_captcha("1221");
+        assert_eq!(0, x);
+        let (_, x) = multi_solve_captcha("123425");
+        assert_eq!(4, x);
+        let (_, x) = multi_solve_captcha("123123");
+        assert_eq!(12, x);
+        let (_, x) = multi_solve_captcha("12131415");
+        assert_eq!(4, x);
     }
 }
