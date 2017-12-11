e9987c444dd6eb233aa68aa148d51277c6724605
tlrobrn <obrien.taylorm@gmail.com>
2017-12-11 16:42:44 -0600

    day2 part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -5,7 +5,10 @@ use std::str::FromStr;
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
     let part1 = checksum(&contents);
+    let part2 = checksum2(&contents);
+
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 fn checksum(contents: &str) -> u64 {
@@ -13,6 +16,13 @@ fn checksum(contents: &str) -> u64 {
         .lines()
         .map(min_max)
         .fold(0, |total, mm| total + (mm.max - mm.min))
+}
+
+fn checksum2(contents: &str) -> u64 {
+    contents
+        .lines()
+        .map(divided_result)
+        .sum()
 }
 
 #[derive(Debug)]
@@ -40,6 +50,21 @@ fn min_max(line: &str) -> MinMax {
     })
 }
 
+fn divided_result(line: &str) -> u64 {
+    let numbers: Vec<u64> = line
+        .split_whitespace()
+        .map(|word| u64::from_str(word).unwrap())
+        .collect();
+
+    for number in &numbers {
+        if let Some(n) = numbers.iter().find(|&x| x != number && (number % x == 0 || x % number == 0)) {
+            return (number / n).max(n / number);
+        }
+    }
+
+    1
+}
+
 #[cfg(test)]
 mod day2_tests {
     use super::*;
@@ -47,5 +72,10 @@ mod day2_tests {
     #[test]
     fn part1() {
         assert_eq!(18, checksum("5 1 9 5\n7 5 3\n2 4 6 8"));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(9, checksum2("5 9 2 8\n 9 4 7 3\n3 8 6 5"));
     }
 }
