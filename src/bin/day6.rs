122cd6a7e5529cfcfa21fb805e2f45d9154a62f0
tlrobrn <obrien.taylorm@gmail.com>
2017-12-14 16:53:39 -0600

    day6 part2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,14 +1,16 @@
 extern crate rusty_the_reindeer;
 
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::iter::FromIterator;
 use std::str::FromStr;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
     let part1 = reallocate(&contents);
+    let part2 = reallocate2(&contents);
 
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 fn reallocate(contents: &str) -> usize {
@@ -23,6 +25,17 @@ fn reallocate(contents: &str) -> usize {
     redistribute(&mut banks, &mut seen, 0)
 }
 
+fn reallocate2(contents: &str) -> usize {
+    let mut banks: Vec<usize> = contents.trim()
+        .split_whitespace()
+        .map(|word| usize::from_str(word).unwrap())
+        .collect();
+
+    let mut seen: HashMap<Vec<usize>, usize> = HashMap::new();
+    seen.insert(banks.to_vec(), 0);
+
+    redistribute2(&mut banks, &mut seen, 0)
+}
 
 fn redistribute(banks: &mut[usize], seen: &mut HashSet<Vec<usize>>, steps: usize) -> usize {
     let length = banks.len();
@@ -47,6 +60,29 @@ fn redistribute(banks: &mut[usize], seen: &mut HashSet<Vec<usize>>, steps: usize
     }
 }
 
+fn redistribute2(banks: &mut[usize], seen: &mut HashMap<Vec<usize>, usize>, steps: usize) -> usize {
+    let length = banks.len();
+    let sorted_banks: BinaryHeap<(usize, usize, usize)> = BinaryHeap::from_iter(
+        banks.iter().enumerate().map(|(i, &v)| (v, length - i, i))
+    );
+
+    let &(blocks, _, index) = sorted_banks.peek().unwrap();
+    banks[index] = 0;
+
+    let mut index = index + 1;
+    for _ in 0..blocks {
+        banks[index % length] += 1;
+        index += 1;
+    }
+
+    if let Some(&original_step) = seen.get(banks) {
+        steps + 1 - original_step
+    } else {
+        seen.insert(banks.to_vec(), steps + 1);
+        redistribute2(banks, seen, steps + 1)
+    }
+}
+
 #[cfg(test)]
 mod day6_tests {
     use super::*;
@@ -54,5 +90,10 @@ mod day6_tests {
     #[test]
     fn part1() {
         assert_eq!(5, reallocate("0 2 7 0"));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(4, reallocate2("0 2 7 0"));
     }
 }
