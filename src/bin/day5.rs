ffbb95445416bf5e88bdb145d94195266c2b19c4
tlrobrn <obrien.taylorm@gmail.com>
2017-12-14 13:27:07 -0600

    day5 part2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -4,13 +4,15 @@ use std::str::FromStr;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
-    let part1 = escape(&contents);
+    let part1 = escape(&contents, i64::max_value());
+    let part2 = escape(&contents, 2);
 
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn escape(contents: &str) -> usize {
-    let mut instructions: Vec<i64> = contents
+fn escape(contents: &str, cutoff: i64) -> usize {
+    let mut offsets: Vec<i64> = contents
         .lines()
         .map(|line| i64::from_str(line.trim()).unwrap())
         .collect();
@@ -18,10 +20,14 @@ fn escape(contents: &str) -> usize {
     let mut position = 0;
     let mut jumps = 0;
 
-    while position < instructions.len() {
-        let instruction = instructions[position];
-        instructions[position] = instruction + 1;
-        position = (position as i64 + instruction) as usize;
+    while position < offsets.len() {
+        let offset = offsets[position];
+        if offset <= cutoff {
+            offsets[position] = offset + 1;
+        } else {
+            offsets[position] = offset - 1;
+        }
+        position = (position as i64 + offset) as usize;
         jumps += 1;
     }
     jumps
