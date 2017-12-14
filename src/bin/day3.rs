9587baa85eaf841efd8b4b7615413bdae44365c9
tlrobrn <obrien.taylorm@gmail.com>
2017-12-13 22:13:19 -0600

    day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -4,9 +4,12 @@ use std::str::FromStr;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
-    let part1 = step_counter(contents.trim());
+    let contents = contents.trim();
+    let part1 = step_counter(contents);
+    let part2 = stress_test(contents);
 
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 fn step_counter(contents: &str) -> i64 {
@@ -63,9 +66,169 @@ fn starting_coords_of(ring: i64) -> (i64, i64) {
     (ring, -ring + 1)
 }
 
+fn stress_test(contents: &str) -> u64 {
+    use spiral::{neighbors, next_coordinate, Spiral};
+
+    let number = u64::from_str(contents).unwrap();
+    let mut spiral = Spiral::default();
+    spiral.push(1);
+
+    loop {
+        if let Some(&value) = spiral.last() {
+            if value > number {
+                return value;
+            }
+        }
+
+        if let Some(coordinate) = next_coordinate(spiral.last_coordinate()) {
+            let value = neighbors(coordinate).iter()
+                .fold(0, |total, coordinate| total + spiral.get_by_coordinate(coordinate).unwrap_or(&0));
+
+            spiral.push(value);
+        }
+    }
+}
+
+
+mod spiral {
+    use std::collections::HashMap;
+
+    type Coordinate = (i64, i64);
+
+    #[derive(Default)]
+    pub struct Spiral<T> {
+        data: Vec<T>,
+        last_coordinate: Option<Coordinate>,
+        coordinate_map: HashMap<Coordinate, usize>,
+    }
+
+    impl <T> Spiral<T> {
+        pub fn push(&mut self, value: T) -> Coordinate {
+            let coordinate = next_coordinate(self.last_coordinate).unwrap();
+
+            self.data.push(value);
+            self.coordinate_map.insert(coordinate, self.data.len());
+            self.last_coordinate = Some(coordinate);
+
+            coordinate
+        }
+
+        pub fn get_by_index(&self, index: usize) -> Option<&T> {
+            if index == 0 {
+                None
+            } else {
+                self.data.get(index - 1)
+            }
+        }
+
+        pub fn get_by_coordinate(&self, coordinate: &Coordinate) -> Option<&T> {
+            self.coordinate_map.get(coordinate).and_then(|&index| self.get_by_index(index))
+        }
+
+        pub fn last_coordinate(&self) -> Option<Coordinate> {
+            self.last_coordinate
+        }
+
+        pub fn last(&self) -> Option<&T> {
+            self.data.last()
+        }
+    }
+
+    pub fn next_coordinate(current_coordinate: Option<Coordinate>) -> Option<Coordinate> {
+        match current_coordinate {
+            None => Some((0, 0)),
+            Some((x, y)) => {
+                let boundary = x.abs().max(y.abs());
+                match (x, y) {
+                    (0, 0) => Some((1, 0)),
+                    (a, b) if a == boundary && b == boundary  => Some((boundary - 1, boundary)),
+                    (a, b) if a == -boundary && b == boundary => Some((-boundary, boundary - 1)),
+                    (a, b) if a == -boundary && b == -boundary => Some((-boundary + 1, -boundary)),
+                    (a, b) if a == boundary && b == -boundary => Some((boundary + 1, -boundary)),
+                    (a, b) if a == boundary => Some((a, b + 1)),
+                    (a, b) if b == boundary => Some((a - 1, b)),
+                    (a, b) if a == -boundary => Some((a, b - 1)),
+                    (a, b) if b == -boundary => Some((a + 1, b)),
+                    _ => None
+                }
+            },
+        }
+    }
+
+    pub fn neighbors(coordinate: Coordinate) -> Vec<Coordinate> {
+        let mut neighbors = Vec::new();
+        for x in -1..2 {
+            for y in -1..2 {
+                if x != 0 || y != 0 {
+                    neighbors.push((coordinate.0 + x, coordinate.1 + y));
+                }
+            }
+        }
+        neighbors
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_push() {
+            let mut spiral = Spiral::default();
+            assert_eq!((0, 0), spiral.push(1));
+            assert_eq!((1, 0), spiral.push(2));
+            assert_eq!((1, 1), spiral.push(3));
+        }
+
+        #[test]
+        fn test_get_by_index_is_one_based() {
+            let mut spiral = Spiral::default();
+            spiral.push(false);
+
+            assert_eq!(None, spiral.get_by_index(0));
+            assert_eq!(Some(&false), spiral.get_by_index(1));
+        }
+
+        #[test]
+        fn test_get_by_index_empty_spiral() {
+            let spiral = Spiral::<bool>::default();
+            assert_eq!(None, spiral.get_by_index(1));
+        }
+
+        #[test]
+        fn test_get_by_coordinate() {
+            let mut spiral = Spiral::default();
+            assert_eq!((0, 0), spiral.push(1));
+            assert_eq!((1, 0), spiral.push(2));
+            assert_eq!((1, 1), spiral.push(3));
+            assert_eq!(Some(&3), spiral.get_by_coordinate(&(1, 1)));
+        }
+
+        #[test]
+        fn test_get_by_coordinate_empty_spiral() {
+            let spiral = Spiral::<bool>::default();
+            assert_eq!(None, spiral.get_by_coordinate(&(1, 1)));
+        }
+
+        #[test]
+        fn test_neighbors() {
+            let mut expected = vec![
+                (-1, 1),  (0, 1),  (1, 1),
+                (-1, 0),           (1, 0),
+                (-1, -1), (0, -1), (1, -1),
+            ];
+            expected.sort();
+
+            let mut actual = neighbors((0, 0));
+            actual.sort();
+
+            assert_eq!(expected, actual);
+        }
+    }
+}
+
 
 #[cfg(test)]
-mod day2_tests {
+mod day3_tests {
     use super::*;
 
     #[test]
