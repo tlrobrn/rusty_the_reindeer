2be85ea86e9c02b277f8b5e55802e99647be5457
tlrobrn <obrien.taylorm@gmail.com>
2017-12-20 15:54:23 -0600

    day9 part2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -4,50 +4,51 @@ use std::str::Chars;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
-    let part1 = score(&contents);
+    let (part1, part2) = score(&contents);
 
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn score(contents: &str) -> u64 {
+fn score(contents: &str) -> (u64, u64) {
     let mut chars = contents.chars();
-    score_loop(&mut chars, 0)
+    score_loop(&mut chars, 0, 0)
 }
 
-fn score_loop(chars: &mut Chars, score: u64) -> u64 {
+fn score_loop(chars: &mut Chars, score: u64, count: u64) -> (u64, u64) {
     match chars.next() {
-        None => score,
-        Some('{') => score_group(chars, score, 0),
+        None => (score, count),
+        Some('{') => score_group(chars, score, count, 0),
         Some('<') => {
-            collect_garbage(chars);
-            score_loop(chars, score)
+            let count = collect_garbage(chars, count);
+            score_loop(chars, score, count)
         },
-        _ => score_loop(chars, score),
+        _ => score_loop(chars, score, count),
     }
 }
 
-fn score_group(chars: &mut Chars, score: u64, depth: u64) -> u64 {
+fn score_group(chars: &mut Chars, score: u64, count: u64, depth: u64) -> (u64, u64) {
     match chars.next() {
-        None => score,
-        Some('}') if depth > 0 => score_group(chars, score + depth + 1, depth - 1),
-        Some('}') => score_loop(chars, score + 1),
-        Some('{') => score_group(chars, score, depth + 1),
+        None => (score, count),
+        Some('}') if depth > 0 => score_group(chars, score + depth + 1, count, depth - 1),
+        Some('}') => score_loop(chars, score + 1, count),
+        Some('{') => score_group(chars, score, count, depth + 1),
         Some('<') => {
-            collect_garbage(chars);
-            score_group(chars, score, depth)
+            let count = collect_garbage(chars, count);
+            score_group(chars, score, count, depth)
         },
-        _ => score_group(chars, score, depth),
+        _ => score_group(chars, score, count, depth),
     }
 }
 
-fn collect_garbage(chars: &mut Chars) {
+fn collect_garbage(chars: &mut Chars, count: u64) -> u64 {
     match chars.next() {
-        None | Some('>') => (),
+        None | Some('>') => count,
         Some('!') => {
             chars.next();
-            collect_garbage(chars)
+            collect_garbage(chars, count)
         },
-        _ => collect_garbage(chars),
+        _ => collect_garbage(chars, count + 1),
     }
 }
 
@@ -58,13 +59,24 @@ mod day9_tests {
 
     #[test]
     fn part1() {
-        assert_eq!(1, score("{}"));
-        assert_eq!(6, score("{{{}}}"));
-        assert_eq!(5, score("{{},{}}"));
-        assert_eq!(16, score("{{{},{},{{}}}}"));
-        assert_eq!(1, score("{<a>,<a>,<a>,<a>}"));
-        assert_eq!(9, score("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
-        assert_eq!(9, score("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
-        assert_eq!(3, score("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+        assert_eq!(1, score("{}").0);
+        assert_eq!(6, score("{{{}}}").0);
+        assert_eq!(5, score("{{},{}}").0);
+        assert_eq!(16, score("{{{},{},{{}}}}").0);
+        assert_eq!(1, score("{<a>,<a>,<a>,<a>}").0);
+        assert_eq!(9, score("{{<ab>},{<ab>},{<ab>},{<ab>}}").0);
+        assert_eq!(9, score("{{<!!>},{<!!>},{<!!>},{<!!>}}").0);
+        assert_eq!(3, score("{{<a!>},{<a!>},{<a!>},{<ab>}}").0);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(0, score("<>").1);
+        assert_eq!(17, score("<random characters>").1);
+        assert_eq!(3, score("<<<<>").1);
+        assert_eq!(2, score("<{!>}>").1);
+        assert_eq!(0, score("<!!>").1);
+        assert_eq!(0, score("<!!!>>").1);
+        assert_eq!(10, score("<{o\"i!a,<{i<a>").1);
     }
 }
