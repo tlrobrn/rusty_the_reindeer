1383b46c99075409db81007f7e670fb93921abc3
tlrobrn <obrien.taylorm@gmail.com>
2017-12-20 16:24:16 -0600

    immutability (and slightly less code)

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,7 +1,5 @@
 extern crate rusty_the_reindeer;
 
-use std::str::Chars;
-
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
     let (part1, part2) = score(&contents);
@@ -11,32 +9,24 @@ fn main() {
 }
 
 fn score(contents: &str) -> (u64, u64) {
-    let mut chars = contents.chars();
-    score_loop(&mut chars, 0, 0, 0)
-}
+    let (_, _, _, score, count) = contents.chars().fold((false, false, 0, 0, 0), |(garbage, skip, depth, score, count), c| {
+        if skip {
+            (garbage, false, depth, score, count)
+        } else {
+            match c {
+                '!' if garbage => (garbage, true, depth, score, count),
+                '>' if garbage => (false, skip, depth, score, count),
+                '{' if !garbage => (false, false, depth + 1, score, count),
+                '}' if !garbage && depth > 0 => (garbage, skip, depth - 1, score + depth, count),
+                '<' if !garbage => (true, skip, depth, score, count),
+                _ if garbage => (garbage, skip, depth, score, count + 1),
+                _ => (garbage, skip, depth, score, count)
+            }
+        }
+    });
 
-fn score_loop(chars: &mut Chars, depth: u64, score: u64, count: u64) -> (u64, u64) {
-    match chars.next() {
-        None => (score, count),
-        Some('{') => score_loop(chars, depth + 1, score, count),
-        Some('}') if depth > 0 => score_loop(chars, depth - 1, score + depth, count),
-        Some('<') => count_garbage(chars, depth, score, count),
-        _ => score_loop(chars, depth, score, count),
-    }
+    (score, count)
 }
-
-fn count_garbage(chars: &mut Chars, depth: u64, score: u64, count: u64) -> (u64, u64) {
-    match chars.next() {
-        None => (score, count),
-        Some('!') => {
-            chars.next();
-            count_garbage(chars, depth, score, count)
-        },
-        Some('>') => score_loop(chars, depth, score, count),
-        _ => count_garbage(chars, depth, score, count + 1),
-    }
-}
-
 
 #[cfg(test)]
 mod day9_tests {
