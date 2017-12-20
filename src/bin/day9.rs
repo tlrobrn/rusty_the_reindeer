3a504dad03a4cee0e17c17e78ca18005366185e3
tlrobrn <obrien.taylorm@gmail.com>
2017-12-20 15:38:15 -0600

    day9 part1

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,5 +1,7 @@
 extern crate rusty_the_reindeer;
 
+use std::str::Chars;
+
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
     let part1 = score(&contents);
@@ -7,9 +9,48 @@ fn main() {
     println!("Part 1: {}", part1);
 }
 
-fn score(_contents: &str) -> u64 {
-    0
+fn score(contents: &str) -> u64 {
+    let mut chars = contents.chars();
+    score_loop(&mut chars, 0)
 }
+
+fn score_loop(chars: &mut Chars, score: u64) -> u64 {
+    match chars.next() {
+        None => score,
+        Some('{') => score_group(chars, score, 0),
+        Some('<') => {
+            collect_garbage(chars);
+            score_loop(chars, score)
+        },
+        _ => score_loop(chars, score),
+    }
+}
+
+fn score_group(chars: &mut Chars, score: u64, depth: u64) -> u64 {
+    match chars.next() {
+        None => score,
+        Some('}') => if depth > 0 { score_group(chars, score + depth + 1, depth - 1) } else { score_loop(chars, score + 1) },
+        Some('{') => score_group(chars, score, depth + 1),
+        Some('<') => {
+            collect_garbage(chars);
+            score_group(chars, score, depth)
+        },
+        _ => score_group(chars, score, depth),
+    }
+}
+
+fn collect_garbage(chars: &mut Chars) {
+    match chars.next() {
+        None => (),
+        Some('>') => (),
+        Some('!') => {
+            chars.next();
+            collect_garbage(chars)
+        },
+        _ => collect_garbage(chars),
+    }
+}
+
 
 #[cfg(test)]
 mod day9_tests {
