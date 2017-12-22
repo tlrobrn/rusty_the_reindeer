30b7ab76dfdb3e8c103e21ba0071705a00ec964d
tlrobrn <obrien.taylorm@gmail.com>
2017-12-22 10:34:53 -0600

    day11 part2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -2,15 +2,29 @@ extern crate rusty_the_reindeer;
 
 fn main() {
     let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
-    let part1 = steps(contents.trim());
+    let part1 = final_steps(contents.trim());
+    let part2 = max_steps(contents.trim());
 
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn steps(contents: &str) -> u64 {
-    let (x, y, z): (i64, i64, i64) = contents.split(',')
-        .fold((0, 0, 0), |(x, y, z), direction| {
-            match direction {
+type Coordinate = (i64, i64, i64);
+
+fn final_steps(contents: &str) -> u64 {
+    build_path(contents).last().map_or(0, distance)
+}
+
+fn max_steps(contents: &str) -> u64 {
+    build_path(contents).iter().map(distance).max().unwrap()
+}
+
+fn build_path(contents: &str) -> Vec<Coordinate> {
+    contents.split(',')
+        .fold(vec![(0, 0, 0)], |mut positions, direction| {
+            let &(x, y, z) = positions.last().unwrap();
+
+            let next_position = match direction {
                 "n" => (x, y + 1, z - 1),
                 "ne" => (x + 1, y, z - 1),
                 "se" => (x + 1, y - 1, z),
@@ -18,9 +32,15 @@ fn steps(contents: &str) -> u64 {
                 "sw" => (x - 1, y, z + 1),
                 "nw" => (x - 1, y + 1, z),
                 _ => (x, y, z),
-            }
-        });
+            };
 
+            positions.push(next_position);
+            positions
+        })
+}
+
+fn distance(coordinate: &Coordinate) -> u64 {
+    let &(x, y, z) = coordinate;
     x.abs().max(y.abs()).max(z.abs()) as u64
 }
 
@@ -30,9 +50,9 @@ mod day11_tests {
 
     #[test]
     fn part1() {
-        assert_eq!(3, steps("ne,ne,ne"));
-        assert_eq!(0, steps("ne,ne,sw,sw"));
-        assert_eq!(2, steps("ne,ne,s,s"));
-        assert_eq!(3, steps("se,sw,se,sw,sw"));
+        assert_eq!(3, final_steps("ne,ne,ne"));
+        assert_eq!(0, final_steps("ne,ne,sw,sw"));
+        assert_eq!(2, final_steps("ne,ne,s,s"));
+        assert_eq!(3, final_steps("se,sw,se,sw,sw"));
     }
 }
