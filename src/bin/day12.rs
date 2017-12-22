fbd34b41d04e4e3d614a1f74c4c5aa99681e35ab
tlrobrn <obrien.taylorm@gmail.com>
2017-12-22 11:57:49 -0600

    day12 part1

---
+++ src/bin/day12.rs
@@ -0,0 +1,71 @@
+extern crate rusty_the_reindeer;
+
+use std::str::FromStr;
+use std::collections::HashSet;
+
+fn main() {
+    let contents = rusty_the_reindeer::get_input().expect("Must provide valid input path");
+    let part1 = count(contents.trim());
+
+    println!("Part 1: {}", part1);
+}
+
+fn count(contents: &str) -> usize {
+    Graph::parse(contents).group(0).len()
+}
+
+#[derive(Default)]
+struct Graph {
+    nodes: Vec<Vec<usize>>,
+}
+
+impl Graph {
+    pub fn parse(input: &str) -> Self {
+        let mut graph = Self::default();
+        for line in input.lines() {
+            let edges: Vec<usize> = line.split("<->")
+                .last()
+                .unwrap()
+                .split(", ")
+                .map(|s| usize::from_str(s.trim()).unwrap())
+                .collect();
+
+            graph.nodes.push(edges)
+        }
+
+        graph
+    }
+
+    pub fn group(&self, node: usize) -> HashSet<usize> {
+        let mut mates = HashSet::new();
+        mates.insert(node);
+        self.build_group(node, &mut mates);
+        mates
+    }
+
+    fn build_group(&self, node: usize, group: &mut HashSet<usize>) {
+        for &mate in &self.nodes[node] {
+            if !group.contains(&mate) {
+                group.insert(mate);
+                self.build_group(mate, group);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod day12_tests {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = "0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+        assert_eq!(6, count(input));
+    }
+}
